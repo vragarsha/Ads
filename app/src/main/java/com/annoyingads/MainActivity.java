34a2b3de0f2923acbdce89a7a28177f33429948d
Ragarsha Velmula <vragarsha@gmail.com>
2018-05-10 21:32:36 -0400

    Added the List View for the Various AdPartners

--- app/src/main/java/com/annoyingads/MainActivity.java
+++ app/src/main/java/com/annoyingads/MainActivity.java
@@ -2,6 +2,8 @@ package com.annoyingads;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView adFormatListView =  findViewById(R.id.adPartners);
+        adFormatListView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.adPartners)));
+
     }
 }
